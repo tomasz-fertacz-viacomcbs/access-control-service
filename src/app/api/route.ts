d40ba00e6c6ef5c253ff7c820c72f44a329e91a0
Tomek Fertacz <tomasz.fertacz@pluto.tv>
2024-04-04 14:57:22 +0200

    add api call

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,11 @@
-export async function POST() {
+/*export async function POST() {
     return Response.json({test: 'test'});
+}*/
+
+import type {NextApiRequest, NextApiResponse} from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        res.json({test: 'test'})
+    }
 }
